e6cd5aaa5956597614bb9fffda267067d349e84d
Jyo Shou-kyou <theorystrong@gmail.com>
2016-03-15 20:09:26 +0900

    commit test

---
+++ japantravel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  japantravel
+//
+//  Created by jyo on 2016/03/15.
+//  Copyright © 2016年 jyo-shoukyou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
